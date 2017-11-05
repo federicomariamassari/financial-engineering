ea37f4bfb4dd9ac65b267e2e5e271f875f68a2e2
Federico Maria Massari <federico.massari@bocconialumni.it>
2017-11-05 18:58:36 +0100

    docs: Add docstrings to jump_diffusion.py

--- python-modules/jump_diffusion.py
+++ python-modules/jump_diffusion.py
@@ -1,30 +1,116 @@
 def jump_diffusion(S0=1, K=0.5, T=1, mu=0.12, sigma=0.3, Lambda=0.25,
                    a=0.2, b=0.2, Nsteps=252, Nsim=100, alpha=0.05):
-    
+    '''
+    Monte Carlo simulation [1] of Merton's Jump Diffusion Model [2].
+    The model is specified through the stochastic differential equation (SDE):
+
+                        dS(t)
+                        ----- = mu*dt + sigma*dW(t) + dJ(t)
+                        S(t-)
+
+    with:
+
+    mu, sigma: constants, the drift and volatility coefficients of the stock
+               price process;
+    W: a standard one-dimensional Brownian motion;
+    J: a jump process, independent of W, with piecewise constant sample paths.
+       It is defined as the sum of multiplicative jumps Y(j).
+
+    Input
+    ---------------------------------------------------------------------------
+    S: float. The current asset price.
+    X: float. The strike price, i.e. the price at which the asset may be bought
+       (call) or sold (put) in an option contract [3].
+    T: int or float. The maturity of the option contract, i.e. the final
+       monitoring date.
+    mu, sigma: float. Respectively, the drift and volatility coefficients of
+               the asset price process.
+    Lambda: float. The intensity of the Poisson process in the jump diffusion
+            model ('lambda' is a protected keyword in Python).
+    a, b: float. Parameters required to calculate, respectively, the mean and
+          variance of a standard lognormal distribution (see code).
+    Nsteps: int. The number of monitoring dates, i.e. the time steps.
+    Nsim: int. The number of Monte Carlo simulations (at least 10,000 required
+          to generate stable results).
+    alpha: float. The confidence interval significance level, in [0, 1].
+
+    References
+    ---------------------------------------------------------------------------
+    [1] Glasserman, P. (2003): 'Monte Carlo Methods in Financial Engineering',
+        Springer Applications of Mathematics, Vol. 53
+    [2] Merton, R.C. (1976): 'Option Pricing when Underlying Stock Returns are
+        Discontinuous', Journal of Financial Economics, 3:125-144.
+    [3] Hull, J.C. (2017): 'Options, Futures, and Other Derivatives', 10th
+        Edition, Pearson.
+    '''
+
+    # Import required libraries
     import time
     import numpy as np
     import matplotlib.pyplot as plt
     import seaborn as sns
 
+    '''
+    Time the whole path-generating process, using a tic-toc method familiar
+    to MATLAB users
+    '''
     tic = time.time()
 
+    # Calculate the length of the time step
     dt = T/Nsteps
 
+    '''
+    Compute mean and variance of a standard lognormal distribution from user
+    defined parameters a and b.
+    Glasserman defines a, b s.t. each multiplicative jump Y(j) ~ LN(a, b**2),
+    i.e. a = mean_Y, b**2 = variance_Y.
+    However, a and b are not independent of each other, as both impact on the
+    definition of the mean and variance of the standard lognormal process.
+    Here, a and b are chosen s.t. log(Y(j)) ~ N(a, b**2), which means Y(j) ~
+    LN(mean_Y, variance_Y), with:
+
+     mean_Y = np.exp(a + 0.5*(b**2))
+     variance_Y = np.exp(2*a + b**2) * (np.exp(b**2)-1)
+    '''
     lognormal_mean = np.exp(a + 0.5*(b**2))
     lognormal_variance = np.exp(2*a + b**2) * (np.exp(b**2)-1)
 
+    '''
+    Calculate drift (M) and volatility (V) of the stock price process under
+    Merton's jump diffusion process.
+    '''
     M = S0 * np.exp(mu*T + Lambda*T*(lognormal_mean-1))
     V = S0**2 * (np.exp((2*mu + sigma**2)*T \
         + Lambda*T*(lognormal_variance + lognormal_mean**2 - 1)) \
         - np.exp(2*mu*T + 2*Lambda*T*(lognormal_mean - 1)))
 
+    '''
+    Generate an Nsim x (Nsteps+1) array of zeros to preallocate the simulated
+    paths of the Monte Carlo simulation. Each row of the matrix represents a
+    full, possible path for the stock, each column all values of the asset at
+    a particular instant in time.
+    '''
     simulated_paths = np.zeros([Nsim, Nsteps+1])
+
+    # Replace the first column of the array with the vector of initial price S
     simulated_paths[:,0] = S0
 
+    '''
+    To account for the multiple sources of uncertainty in the jump diffusion
+    process, generate three arrays of random variables.
+
+     - The first one is related to the standard Brownian motion, the component
+       epsilon(0,1) in epsilon(0,1) * np.sqrt(dt);
+     - The second and third ones model the jump, a compound Poisson process:
+       the former (a Poisson process with intensity Lambda) causes the asset
+       price to jump randomly (random timing); the latter (a Gaussian variable)
+       defines both the direction (sign) and intensity (magnitude) of the jump.
+    '''
     normal_rv = np.random.normal(size=[Nsim, Nsteps])
     normal_rv2 = np.random.normal(size=[Nsim, Nsteps])
     poisson = np.random.poisson(Lambda*dt, [Nsim, Nsteps])
 
+    # Populate the matrix with Nsim randomly generated paths of length Nsteps
     for i in range(Nsteps):
         simulated_paths[:,i+1] = simulated_paths[:,i]*np.exp((mu
                                - sigma**2/2)*dt + sigma*np.sqrt(dt) \
@@ -32,23 +118,31 @@ def jump_diffusion(S0=1, K=0.5, T=1, mu=0.12, sigma=0.3, Lambda=0.25,
                                + np.sqrt(b**2) * np.sqrt(poisson[:,i]) \
                                * normal_rv2[:,i])
 
+    # Choose palette, figure size, and define figure axes
     sns.set(palette='viridis')
     plt.figure(figsize=(10,8))
     ax = plt.axes()
 
+    # Generate t, the time variable on the abscissae
     t = np.linspace(0, T, Nsteps+1) * Nsteps
+
+    # Plot the Monte Carlo simulated stock price paths
     jump_diffusion = ax.plot(t, simulated_paths.transpose());
 
+    # Make drawn paths thinner by decreasing line width
     plt.setp(jump_diffusion, linewidth=1);
 
+    # Set title (LaTeX notation) and x- and y- labels
     ax.set(title="Monte Carlo simulated stock price paths in Merton's jump \
 diffusion model\n$S_0$ = {}, $\mu$ = {}, $\sigma$ = {}, $\mu_Y$ = {}, \
 $\sigma_Y$ = {}, $\lambda$ = {}, $T$ = {}, Nsteps = {}, Nsim = {}"\
            .format(S0, mu, sigma, a, b, Lambda, T, Nsteps, Nsim), \
            xlabel='Time (days)', ylabel='Stock price')
 
+    # Display figure in a Python environment
     plt.show()
 
+    # Time and print the elapsed time
     toc = time.time()
     elapsed_time = toc - tic
     print('Total running time: {:.2f} ms'.format(elapsed_time*1000))
